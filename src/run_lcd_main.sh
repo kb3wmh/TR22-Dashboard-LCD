ca16fa1fdc775a74c9da640fee7ce07331b28f1e
Charlie Dawson <charliedawson@Charlies-MacBook-Pro-16.local>
2023-04-12 22:47:28 -0400

    Added script to run LCD_main in the background

---
+++ src/run_lcd_main.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+LCD_MAIN=$SCRIPTPATH"/LCD_Main"
+
+if [ -f $LCD_MAIN ]
+then
+    nohup $LCD_MAIN &> /tmp/lcd.log &
+
+else
+    echo "File: "$LCD_MAIN" does not exist. Did you compile the program?"
+fi
